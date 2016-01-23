dddd70444351da970f97ab00654116f3e5ff7d7b
Robbert Brandsma <i@robbertbrandsma.nl>
2016-01-23 13:11:18 +0100

    Remove template stuff

--- BSONTests/BSONTests.swift
+++ BSONTests/BSONTests.swift
@@ -11,16 +11,6 @@ import XCTest
 
 class BSONTests: XCTestCase {
     
-    override func setUp() {
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-    
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
-    }
-    
     func testDoubleSerialization() {
         // This is 5.05
         let rawData: [UInt8] = [0x33, 0x33, 0x33, 0x33, 0x33, 0x33, 0x14, 0x40]
@@ -29,13 +19,6 @@ class BSONTests: XCTestCase {
         
         let generatedData = 5.05.bsonData
         XCTAssert(generatedData == rawData, "Converting a Double to BSON data results in the correct data")
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measureBlock {
-            // Put the code you want to measure the time of here.
-        }
     }
     
 }
