c29c722f1abac9372b5eb984f9f2c50469bff736
Joannis Orlandos <joannis@orlandos.nl>
2016-01-26 16:22:03 +0100

    Now really added random ObjectID

--- BSON/Integer16.swift
+++ BSON/Integer16.swift
@@ -1,9 +1,34 @@
 //
-//  Integer16.swift
+//  Boolean.swift
 //  BSON
 //
-//  Created by Joannis Orlandos on 26/01/16.
+//  Created by Joannis Orlandos on 23/01/16.
 //  Copyright © 2016 Robbert Brandsma. All rights reserved.
 //
 
 import Foundation
+
+extension Int16 {
+    public static func instantiate(bsonData data: [UInt8]) throws -> Int16 {
+        var ditched = 0
+        
+        return try instantiate(bsonData: data, consumedBytes: &ditched)
+    }
+    
+    public static func instantiate(bsonData data: [UInt8], inout consumedBytes: Int) throws -> Int16 {
+        guard data.count == 2 else {
+            throw DeserializationError.InvalidElementSize
+        }
+        
+        let integer = UnsafePointer<Int16>(data).memory
+        consumedBytes = 2
+        return integer
+    }
+    
+    public var bsonData: [UInt8] {
+        var integer = self
+        return withUnsafePointer(&integer) {
+            Array(UnsafeBufferPointer(start: UnsafePointer<UInt8>($0), count: sizeof(Int16)))
+        }
+    }
+}
--- BSON/ObjectID.swift
+++ BSON/ObjectID.swift
@@ -9,8 +9,9 @@
 import Foundation
 
 public struct ObjectID {
-    
     public private(set) var data: [UInt8]
+    private static var random = UInt8(arc4random_uniform(255))
+    private static var counter: Int16 = 0
     
     public init(hexString: String) throws {
         data = hexString.characters.map { UInt8(String($0), radix: 16) }.flatMap{$0}
@@ -20,7 +21,67 @@ public struct ObjectID {
         }
     }
     
+    public init(bsonData: [UInt8]) throws {
+        data = bsonData
+        
+        guard data.count == 12 else {
+            throw DeserializationError.ParseError
+        }
+    }
+    
     public var hexString: String {
         return data.map{String($0, radix: 16, uppercase: false)}.joinWithSeparator("")
+    }
+    
+    public static func generate() throws -> ObjectID {
+        let currentTime = NSDate()
+        
+        var data = [UInt8]()
+        
+        // Take the current UNIX epoch as Int32 and take it's bytes
+        data += try Int32.instantiate(bsonData: Array(currentTime.bsonData[0...3])).bsonData
+        
+        // Take the machine identifier
+        // TODO: Change this to a MAC address
+        data += Array(NSProcessInfo.processInfo().hostName.hash.bsonData[0...2])
+        
+        // Take the process identifier as 2 bytes
+        data += Array(NSProcessInfo.processInfo().processIdentifier.bsonData[0...1])
+        
+        // Take a random number
+        data += [random]
+        
+        // And add a counter as 2 bytes and increment it
+        data += counter.bsonData
+        counter += 1
+        
+        return try self.init(bsonData: data)
+    }
+}
+
+extension ObjectID : BSONElementConvertible {
+    public var elementType: ElementType {
+        return .ObjectId
+    }
+    
+    /// Here, return the same data as you would accept in the initializer
+    public var bsonData: [UInt8] {
+        return data
+    }
+    
+    public static var bsonLength: BsonLength {
+        return .Fixed(length: 12)
+    }
+    
+    /// The initializer expects the data for this element, starting AFTER the element type
+    public static func instantiate(bsonData data: [UInt8], inout consumedBytes: Int) throws -> ObjectID {
+        let objectID = try self.init(bsonData: data)
+        consumedBytes = data.count
+        
+        return objectID
+    }
+    
+    public static func instantiate(bsonData data: [UInt8]) throws -> ObjectID {
+        return try self.init(bsonData: data)
     }
 }
