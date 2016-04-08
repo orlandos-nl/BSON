1e57fc63fa2e0564657b1eedbfe3c3c954ff23c0
Robbert Brandsma <i@robbertbrandsma.nl>
2016-04-08 13:19:02 +0200

    Fixed the behavior of document + document so it doesn't create an invalid document with duplicate keys

--- Sources/Document+Operators.swift
+++ Sources/Document+Operators.swift
@@ -8,19 +8,31 @@
 
 import Foundation
 
+/// Combines the left and right document.
+/// If the documents are arrays, the right document will be appended at the end of the left document.
+/// If the documents aren't arrays, the value for the right document will be used for any key contained in both documents.
 public func +(lhs: Document, rhs: Document) -> Document {
     let areArrays = lhs.validatesAsArray() && rhs.validatesAsArray()
     
-    var lhs = lhs
-    lhs.elements += rhs.elements
-    
+    var new = lhs
     if areArrays {
-        lhs.enforceArray()
-    }
+        new.elements += rhs.elements
     
-    return lhs
+        new.enforceArray()
+        
+        return new
+    } else {
+        for (key, value) in rhs.elements {
+            if let index = new.elements.index(where: {$0.0 == key}) {
+                new.elements.remove(at: index)
+            }
+            
+            new.elements.append((key, value))
+        }
+        return new
+    }
 }
 
-public func +=( lhs: inout Document, rhs: Document) {
-    lhs.elements += rhs.elements
+public func +=(lhs: inout Document, rhs: Document) {
+    lhs = lhs + rhs
 }
