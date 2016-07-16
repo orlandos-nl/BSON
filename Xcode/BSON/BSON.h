da4a398c9fc1f0df9d3f586a13c699a215fe906c
Alsey Coleman Miller <alseycmiller@gmail.com>
2016-07-16 10:58:44 -0500

    Created Xcode project
    For Carthage support

---
+++ Xcode/BSON/BSON.h
@@ -0,0 +1,19 @@
+//
+//  BSON.h
+//  BSON
+//
+//  Created by Alsey Coleman Miller on 7/16/16.
+//  Copyright © 2016 OpenKitten. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for BSON.
+FOUNDATION_EXPORT double BSONVersionNumber;
+
+//! Project version string for BSON.
+FOUNDATION_EXPORT const unsigned char BSONVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BSON/PublicHeader.h>
+
+
