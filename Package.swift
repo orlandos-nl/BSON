c1a232876f0b78c10d4e11ceb797e23898881fbd
Robbert Brandsma <i@robbertbrandsma.nl>
2016-07-29 11:51:35 +0200

    Fix open-swift url in Package.swift

--- Package.swift
+++ Package.swift
@@ -7,6 +7,6 @@ import PackageDescription
 let package = Package(
     name: "BSON",
     dependencies: [
-        .Package(url: "https://github.com/Open-Swift/C7.git", majorVersion: 0, minor: 10)
+        .Package(url: "https://github.com/open-swift/C7.git", majorVersion: 0, minor: 10)
     ]
 )
