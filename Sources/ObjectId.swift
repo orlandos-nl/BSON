a56e21092c784846c7738c7143575fb091a2b0d8
Joannis Orlandos <j.orlandos@jakajima.eu>
2016-09-26 16:29:30 +0200

    ObjectId made hashable

--- Sources/ObjectId.swift
+++ Sources/ObjectId.swift
@@ -122,4 +122,11 @@ public struct ObjectId {
     }
 }
 
+extension ObjectId: Hashable {
+    /// The hash value for this ObjectId is currently the hexstring. Will be more performant in the future
+    public var hashValue: Int {
+        return self.hexString.hashValue
+    }
+}
+
 private let radix16table: [UInt8] = [0x30, 0x31, 0x32, 0x33, 0x34, 0x35, 0x36, 0x37, 0x38, 0x39, 0x61, 0x62, 0x63, 0x64, 0x65, 0x66]
