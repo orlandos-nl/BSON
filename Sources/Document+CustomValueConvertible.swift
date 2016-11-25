29f087ba381ba25daa53dd9aefbb1adf573ddd3e
Joannis Orlandos <joannis@orlandos.nl>
2016-11-25 21:01:44 +0100

    Extracting using SubscriptExpressionType

--- Sources/Document+CustomValueConvertible.swift
+++ Sources/Document+CustomValueConvertible.swift
@@ -1,5 +1,5 @@
 extension Document {
-    public func extract<V: CustomValueConvertible>(_ key: String...) -> V? {
+    public func extract<V: CustomValueConvertible>(_ key: SubscriptExpressionType...) -> V? {
         guard let primitive = self[key]?.makeBSONPrimitive() else {
             return nil
         }
