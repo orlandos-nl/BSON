703b2ba10b4d55100a097a13d4b6a61fc57f9a59
Robbert Brandsma <i@robbertbrandsma.nl>
2017-02-13 14:08:57 +0100

    Removed BSON 4 unit tests

---
+++ Tests/BSONTests/BSONTests.swift
@@ -0,0 +1,26 @@
+//
+//  BSONTests.swift
+//  BSONTests
+//
+//  Created by Robbert Brandsma on 23-01-16.
+//  Copyright © 2016 Robbert Brandsma. All rights reserved.
+//
+
+import Foundation
+import XCTest
+@testable import BSON
+
+#if os(Linux)
+    import Glibc
+#endif
+
+final class BSONPublicTests: XCTestCase {
+    
+    // TODO: Re-write unit tests
+    
+    static var allTests : [(String, (BSONPublicTests) -> () throws -> Void)] {
+        return [
+            
+        ]
+    }
+}
