0d21b908eedc4661199d616a3fbbfa53737d28cd
Robbert Brandsma <i@robbertbrandsma.nl>
2017-05-18 15:02:14 +0200

    ObjectId CustomStringConvertible

--- Sources/BSON/ObjectId.swift
+++ Sources/BSON/ObjectId.swift
@@ -165,4 +165,10 @@ extension ObjectId: Hashable {
     }
 }
 
+extension ObjectId : CustomStringConvertible {
+    public var description: String {
+        return "ObjectId(\"\(hexString)\")"
+    }
+}
+
 private let radix16table: Bytes = [0x30, 0x31, 0x32, 0x33, 0x34, 0x35, 0x36, 0x37, 0x38, 0x39, 0x61, 0x62, 0x63, 0x64, 0x65, 0x66]
