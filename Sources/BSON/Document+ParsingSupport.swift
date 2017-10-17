de12565940de3aaf456c74eb68be6c8ef67e367b
Joannis Orlandos <joannis@orlandos.nl>
2017-10-17 13:11:56 +0200

    add count shortcut preventing re-indexing

--- Sources/BSON/Document+ParsingSupport.swift
+++ Sources/BSON/Document+ParsingSupport.swift
@@ -179,7 +179,7 @@ extension Document {
         
         if let keys = keys {
             self.searchTree[keys]?.fullyIndexed = true
-        } else if levels == nil {
+        } else if levels == nil || levels == 0 {
             self.searchTree.fullyIndexed = self.searchTree.storage.values.reduce(true) { $0.1.fullyIndexed && $0.0 }
         }
         
