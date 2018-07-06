383f18b0d18e50f6c2cb8a22ef1c116b474b2e90
Robbert Brandsma <i@robbertbrandsma.nl>
2018-07-07 00:29:13 +0200

    Updated Package.swift

--- Package.swift
+++ Package.swift
@@ -1,9 +1,20 @@
-//
-// Created by joannis on 27-1-16.
-//
+// swift-tools-version:4.2
 
 import PackageDescription
 
 let package = Package(
-    name: "BSON"
+    name: "BSON",
+    products: [
+        .library(
+            name: "BSON",
+            targets: ["BSON"])
+        ],
+    targets: [
+        .target(
+            name: "BSON"),
+        .testTarget(
+            name: "BSONTests",
+            dependencies: ["BSON"]),
+        ],
+    swiftLanguageVersions: [.v4_2]
 )
