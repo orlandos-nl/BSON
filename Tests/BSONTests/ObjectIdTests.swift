2c485d11bf20127573707c3d41041bbfdad38825
Robbert Brandsma <i@robbertbrandsma.nl>
2018-10-15 11:35:59 +0200

    Timestamp test

---
+++ Tests/BSONTests/ObjectIdTests.swift
@@ -0,0 +1,38 @@
+//
+//  ObjectIdTests.swift
+//  BSONTests
+//
+//  Created by Robbert Brandsma on 15/10/2018.
+//
+
+import XCTest
+import BSON
+
+class ObjectIdTests: XCTestCase {
+
+    func testTimestamp() {
+        // https://github.com/mongodb/specifications/blob/6a31988385f54e4ec9daddfbf21344a2a96e2656/source/objectid.rst#id7
+        let caseA = try! ObjectId("00000000AAAAAAAAAAAAAAAA")
+        let caseB = try! ObjectId("7FFFFFFFAAAAAAAAAAAAAAAA")
+        let caseC = try! ObjectId("80000000AAAAAAAAAAAAAAAA")
+        let caseD = try! ObjectId("FFFFFFFFAAAAAAAAAAAAAAAA")
+        
+        let dateA = Date(timeIntervalSince1970: 0x00000000)
+        let dateB = Date(timeIntervalSince1970: 0x7FFFFFFF)
+        let dateC = Date(timeIntervalSince1970: 0x80000000)
+        let dateD = Date(timeIntervalSince1970: 0xFFFFFFFF)
+        
+        XCTAssertEqual(caseA.timestamp, 0x00000000)
+        XCTAssertEqual(caseA.date, dateA)
+        
+        XCTAssertEqual(caseB.timestamp, 0x7FFFFFFF)
+        XCTAssertEqual(caseB.date, dateB)
+        
+        XCTAssertEqual(caseC.timestamp, 0x80000000)
+        XCTAssertEqual(caseC.date, dateC)
+        
+        XCTAssertEqual(caseD.timestamp, 0xFFFFFFFF)
+        XCTAssertEqual(caseD.date, dateD)
+    }
+
+}
