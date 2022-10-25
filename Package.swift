9ebd85bbbee16cefd1cf9be96cfc56d03eeb0520
taylorswift <kelvin13ma@gmail.com>
2022-10-25 12:27:46 -0500

    align with project style

--- Package.swift
+++ Package.swift
@@ -1,34 +1,31 @@
 // swift-tools-version:5.5
+
 import PackageDescription
 
-let package:Package = .init(
-    name: "swift-bson",
-    platforms: 
-    [
+let package = Package(
+    name: "BSON",
+    platforms: [
         .macOS(.v10_15),
         .iOS(.v13)
     ],
-    products: 
-    [
-        .library(name: "BSON", targets: ["BSON"])
-    ],
-    dependencies: 
-    [
+    products: [
+        .library(
+            name: "BSON", 
+            targets: ["BSON"])
+        ],
+    dependencies: [
         .package(url: "https://github.com/apple/swift-nio.git", from: "2.0.0")
     ],
-    targets: 
-    [
-        .target(name: "BSON", 
-            dependencies: 
-            [
+    targets: [
+        .target(
+            name: "BSON", 
+            dependencies: [
                 .product(name: "NIOCore", package: "swift-nio"),
-            ]),
-        
-        .testTarget(name: "BSONTests",
-            dependencies: 
-            [
-                .target(name: "BSON")
-            ]),
-    ],
+            ]
+        ),
+        .testTarget(
+            name: "BSONTests",
+            dependencies: ["BSON"]),
+        ],
     swiftLanguageVersions: [.v4_2]
 )
